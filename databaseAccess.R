f3c5e7d8e76f3911d9982d3fee7a6bb4bd95cf39
maruhnth <thomas.maruhn@gmail.com>
2016-03-09 14:57:03 +0100

    Read views from database

--- databaseAccess.R
+++ databaseAccess.R
@@ -29,4 +29,17 @@ for (i in 1:length(tables)) {
   assign(paste(tables[i]), dbReadTable(con, c("config_data",tables[i])))
 }
 
+# list all views
+query_views <- dbGetQuery(con,"SELECT viewname from pg_catalog.pg_views")
+
+# extract view names related to 'view_configuration'
+view_configurations <- grep("view_configuration",query_views$viewname)
+view_configurations <- query_views[view_configurations,]
+
+# read view_configurations
+# for sake of convenience, use only substring as name without 'view_configuration_' prefix
+for (i in 1:length(view_configurations)) {
+  assign(paste(gsub("view_configuration_","", view_configurations[i])), dbReadTable(con, c("config_data",view_configurations[i])))
+}
+
 dbDisconnect(con)
